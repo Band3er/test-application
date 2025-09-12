5b7174d150eadb9b015881e71eae377c43ffa031
spyrex <69646109+Band3er@users.noreply.github.com>
2025-09-12 20:21:07 +0300

    added test

---
+++ test/widget_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_application/myHomePage.dart';
+
+void main() {
+  testWidgets('it renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MyHomePage(title: 'Test Application'),
+    ));
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Hello there'), findsOneWidget);
+    expect(find.text('#ffffff'), findsOneWidget);
+  });
+}
