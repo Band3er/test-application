2b4eb9bd84051a762931361a7ee82c7a53de0cb7
spyrex <69646109+Band3er@users.noreply.github.com>
2025-09-12 19:27:59 +0300

    added features

--- lib/main.dart
+++ lib/main.dart
@@ -32,25 +32,47 @@ class _MyHomePageState extends State<MyHomePage> {
   double? xCoordinate = 0;
   double? yCoordinate = 0;
 
+  List<Map<String, int>> colorHistory = [
+    {'red': 255, 'green': 255, 'blue': 255},
+  ];
+
   @override
   void initState() {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
 
+    final hexColor =
+        "${colorHistory.last['red']?.toRadixString(16)}${colorHistory.last['green']?.toRadixString(16)}${colorHistory.last['blue']?.toRadixString(16)}";
+
+    print(colorHistory.toString());
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_return),
+          onPressed: () => _previousColor(colorHistory),
+        ),
         title: Text(widget.title),
       ),
-      backgroundColor: Color.fromRGBO((xCoordinate! % 256).round(), (yCoordinate! % 256).round(), ((xCoordinate! * yCoordinate!) % 256).round(), 1),
+      backgroundColor: _setColor(
+        colorHistory.last['red']!,
+        colorHistory.last['green']!,
+        colorHistory.last['blue']!,
+      ),
       body: GestureDetector(
         child: Center(
-          child: Text("Hello there", style: TextStyle(fontSize: width * 0.1)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Hello there", style: TextStyle(fontSize: width * 0.1)),
+              Text("#$hexColor", style: TextStyle(fontSize: width * 0.08)),
+            ],
+          ),
         ),
         behavior: HitTestBehavior.opaque,
         onTapDown: (TapDownDetails details) => _onTapDown(details),
@@ -59,11 +81,34 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _onTapDown(TapDownDetails details) {
+    xCoordinate = details.globalPosition.dx;
+    yCoordinate = details.globalPosition.dy;
+
+    final colorRed = xCoordinate!.round() % 256;
+    final colorGreen = yCoordinate!.round() % 256;
+    final colorBlue = (xCoordinate! * yCoordinate!).round() % 256;
+
     setState(() {
-      xCoordinate = details.globalPosition.dx;
-      yCoordinate = details.globalPosition.dy;
+      colorHistory.add({
+        'red': colorRed,
+        'green': colorGreen,
+        'blue': colorBlue,
+      });
     });
 
     print("dx: $xCoordinate \n dy: $yCoordinate");
+  }
+
+  Color? _setColor(int colorRed, int colorGreen, int colorBlue) {
+    return Color.fromRGBO(colorRed, colorGreen, colorBlue, 1);
+  }
+
+  _previousColor(List<dynamic> colorHistory) {
+    setState(() {
+      if (colorHistory.length > 1) {
+        colorHistory.removeLast();
+        print(colorHistory.toString());
+      }
+    });
   }
 }
